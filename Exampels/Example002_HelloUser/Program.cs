3fdf3f2e9186d417930d578bf0487ba0b2aac9c1
Yuriy Emelin <yuem-1@yandex.ru>
2023-06-24 21:05:59 +0300

    Гот!

--- Exampels/Example002_HelloUser/Program.cs
+++ Exampels/Example002_HelloUser/Program.cs
@@ -1,8 +1,2 @@
-﻿Console.WriteLine("Введите ваше имя ");
-string username = Console.ReadLine();
-Console.Write("Привет, ")  ;
-Console.Write(username);
-
-//● WriteLine — вывод в консоль с переходом на новую строку.
-//● Write — это вывод в консоль без перехода на новую строку.
-//● ReadLine — позволяет считывать данные из нашего терминала.
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
