2eb724ad2518e0464a1f61035080cad1810f3575
Yuriy Emelin <yuem-1@yandex.ru>
2023-06-24 20:16:31 +0300

    Гот!

--- Exampels/Example002_HelloUser/Program.cs
+++ Exampels/Example002_HelloUser/Program.cs
@@ -2,7 +2,7 @@
 string username = Console.ReadLine();
 Console.Write("Привет,")  ;
 Console.Write(username);
-Console.Write(" !")
+Console.Write(" !");
 //● WriteLine — вывод в консоль с переходом на новую строку.
 //● Write — это вывод в консоль без перехода на новую строку.
 //● ReadLine — позволяет считывать данные из нашего терминала.
