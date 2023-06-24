7614632661f38f2af17044210c86fec4d03b086f
Yuriy Emelin <yuem-1@yandex.ru>
2023-06-24 20:19:43 +0300

    Гот!

--- Exampels/Example002_HelloUser/Program.cs
+++ Exampels/Example002_HelloUser/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("Введите ваше имя ");
 string username = Console.ReadLine();
-Console.Write("Привет,")  ;
-Console.Write(username);
+Console.Write("Привет, ")  ;
+Console.Write(username,"!");
 
 //● WriteLine — вывод в консоль с переходом на новую строку.
 //● Write — это вывод в консоль без перехода на новую строку.
