96c1a816d8f608f2b3d75a3daaac62e83ec9de47
Yuriy Emelin <yuem-1@yandex.ru>
2023-06-24 16:38:18 +0300

    Exampel001_console

---
+++ Exampels/Example001_HelloConsole/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Мир!");
+Console.WriteLine("Hello, Мир!");
+Console.WriteLine("Hello, Мир!");
+//Dotnet new console — эта команда нужна, чтобы создать новый проект.
+//● Dotnet run — эта команда нужна, чтобы запустить проект. Позднее я покажу, что будет, если у
+//вас возникают какие-то ошибки, как нужно будет действовать и каким образом их чинить.
+//● Через Console мы обращаемся к окну терминала.
+//● С помощью «WriteLine» можем в консоли данные выводить.
